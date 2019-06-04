7e4fa4fcaaae667fb32e12b7e617747876583ec3
Gael MOTTE <gael.motte@gmail.com>
2019-06-04 15:22:59 +0200

    slight change

--- server.js
+++ server.js
@@ -5,7 +5,29 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+/*server.use((req, res, next) => {
+	if (req.headers.authorization) { 
+
+		let user, password;
+		[user, password] = (new Buffer(req.headers.authorization.split(" ")[1], 'base64').toString()).split(":");
+		
+		if(user === "root" && password === "root"){
+			next()
+		}else{
+			console.log(router.db.users)
+			res.sendStatus(401)
+		}
+
+	
+	} else {
+		res.sendStatus(401)
+	}
+})
+*/
+
+
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running on ', process.env.PORT)
 })

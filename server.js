926d488919a3945bbd231fb6bc749719b5d89faf
Gael MOTTE <gael.motte@gmail.com>
2019-06-05 18:20:46 +0200

    securing operations

--- server.js
+++ server.js
@@ -70,8 +70,6 @@ server.use((req, res, next) => {
 		next()
 	}else{
 
-
-		
 		if(req.method === "GET"){//add a filter in get queries
 			console.log("query : ",req.query)
 			req.query.customer_id = ""+req.customer.id
@@ -79,25 +77,50 @@ server.use((req, res, next) => {
 			console.log("query : ",req.query)
 			next()
 
-		}else{//prevent access if not owned by correct customer
-			//TODO
+		}else if (req.method === "POST"){//
 
 			//force appending customer_id
 			req.body.customer_id = req.customer.id
 
 			next()
 
+		}else{
+			//prevent access if not owned by correct customer
+			//done in the following lines
+			next()
 		}
-
-
-		
-
-
 	}
 });
 
+function secureRoute(resource){
 
-server.use(router)
+	return function (req, res, next) {
+
+		let elem = router.db.get(resource).filter({"id":parseInt(req.params.id)}).nth(0).value();
+	  if(elem && elem.customer_id === req.customer.id){
+			next()
+	  }else{
+	  	res.sendStatus(403)
+	  }
+	};
+}
+
+server.patch("/api/leads/:id", secureRoute("leads"));
+server.patch("/api/opportunities/:id", secureRoute("opportunities"));
+server.patch("/api/events/:id", secureRoute("events"));
+
+server.put("/api/leads/:id", secureRoute("leads"));
+server.put("/api/opportunities/:id", secureRoute("opportunities"));
+server.put("/api/events/:id", secureRoute("events"));
+
+server.delete("/api/leads/:id", secureRoute("leads"));
+server.delete("/api/opportunities/:id", secureRoute("opportunities"));
+server.delete("/api/events/:id", secureRoute("events"));
+
+server.use("/api/",router)
+
+
+
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running on ', process.env.PORT || 3000)
 })

b72f6c64d4a702f041bc0b80d436558474fa2673
Gael MOTTE <gael.motte@gmail.com>
2019-06-08 17:02:58 +0200

    Model refactor (#3)
    * modularization
    
    * per roganziation oauth settup
    
    * removing deadcode
    
    * type in route
    
    * type conversion fail
    
    * type conversion fail
    
    * dead code removal

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 // server.js
 const jsonServer = require('json-server')
+const securitysetup = require('./securitysetup')
+const jsforce = require('jsforce')
+const randomstring = require('randomstring')
+
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
 const router = jsonServer.router({
-	"customers":[
+	"organization":[
 		{
 			"id":0,
-			"username":"cust0",
-			"password":"cust0"
+			"orgName":"StartUp0"
+			//SFDC related info goes here
+		},
+		{
+			"id":1,
+			"orgName":"StartUp1"
+			//SFDC related info goes here
 		}
 	],
-	"administrators":[
+	"user":[
+		{
+			"id":0,
+			"username":"user0",
+			"password":"user0",
+			"organizationId":0
+		}
+	],
+	"administrator":[
 		{
 			"id":0,
 			"username":"root",
@@ -20,12 +37,12 @@ const router = jsonServer.router({
 	"leads":[
 		{
 			"id":0,
-			"customer_id":0,
+			"organizationId":0,
 			"name":"lead0"
 		},
 		{
 			"id":1,
-			"customer_id":1,
+			"organizationId":1,
 			"name":"lead1"
 		}
 	],
@@ -36,94 +53,12 @@ const router = jsonServer.router({
 
 
 server.use(middlewares)
-
 server.use(jsonServer.bodyParser)
-
-
-/* AuthN middleware */
-server.use("/api/*",(req, res, next) => {
-	if (req.headers.authorization) { 
-
-
-		let username, password;
-		[username, password] = (new Buffer(req.headers.authorization.split(" ")[1], 'base64').toString()).split(":");
-		let administrator = router.db.get("administrators").value().filter( u => u.username === username && u.password === password)[0];
-
-		if(administrator){
-			req.administrator = administrator
-			console.log("acting as aadministrator", administrator)
-			next()
-		}else{
-			let customer = router.db.get("customers").value().filter( u => u.username === username && u.password === password)[0];
-			if(customer){
-				req.customer = customer
-				console.log("acting as customer", customer)
-				next()
-			}else{
-				res.sendStatus(401)
-			}
-		}
-	} else {
-		res.sendStatus(401)
-	}
-})
-
-/* AuthZ middleware */
-server.use("/api/*",(req, res, next) => {
-	if(!req.customer){
-		next()
-	}else{
-
-
-		
-		if(req.method === "GET"){//add a filter in get queries
-			console.log("query : ",req.query)
-			req.query.customer_id = ""+req.customer.id
-
-			console.log("query : ",req.query)
-			next()
-
-		}else{//prevent access if not owned by correct customer
-
-			//force appending customer_id
-			req.body.customer_id = req.customer.id
-
-			next()
-
-		}
-	}
-});
-
-function secureRoute(resource){
-	return function (req, res, next) {
-		console.log("secrutity Middleware executed")
-		let elem = router.db.get(resource).filter({"id":parseInt(req.params.id)}).nth(0).value();
-		if(elem && elem.customer_id === req.customer.id){
-			next()
-		}else{
-			res.sendStatus(403)
-		}
-	}
-}      
-
-server.all("/api/leads/:id", secureRoute("leads"));
-server.all("/api/opportunities/:id", secureRoute("opportunities"));
-server.all("/api/events/:id", secureRoute("events"));
-
-
-
+securitysetup(server,router)
 server.use("/api",router)
 
 
 
-var jsforce = require('jsforce');
-
-
-var SFOauth = {
-	instanceUrl : 'https://heydemo-dev-ed.lightning.force.com',
-  accessToken : null,//'<your Salesforrce OAuth2 access token is here>',
-  refreshToken : null//'<your Salesforce OAuth2 refresh token is here>'
-}
 
 var oauth2 = new jsforce.OAuth2({
   // you can change loginUrl to connect to sandbox or prerelease env.
@@ -134,21 +69,10 @@ var oauth2 = new jsforce.OAuth2({
 });
 
 
-var conn = new jsforce.Connection({
-  oauth2 ,
-  instanceUrl : SFOauth.instanceUrl,
-  accessToken : SFOauth.accessToken,
-  refreshToken : SFOauth.refreshToken
-});
-conn.on("refresh", function(accessToken, res) {
-  // Refresh event will be fired when renewed access token
-  // to store it in your storage for next request
-  console.log("refresh access token", accessToken);
-  SFOauth.accessToken = accessToken;
-});
-
-server.get('/oauth2/auth', function(req, res) {
-  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api' }));
+server.get('/oauth2/auth/:oid', function(req, res) {
+	let state = randomstring.generate()
+	console.log(router.db.get("organization").filter({"id": parseInt(req.params.oid)}).nth(0).assign({sfdc_oauthState:state}).write())
+	res.redirect(oauth2.getAuthorizationUrl({ scope : 'api' })+"&state="+state);
 });
 
 //
@@ -157,22 +81,51 @@ server.get('/oauth2/auth', function(req, res) {
 server.get('/oauth2/callback', function(req, res) {
   var conn = new jsforce.Connection({ oauth2 : oauth2 });
   var code = req.param('code');
+  var state = req.param("state")
+  console.log(state)
+
   conn.authorize(code, function(err, userInfo) {
     if (err) { return console.error(err); }
     // Now you can get the access token, refresh token, and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    SFOauth.accessToken = conn.accessToken;
-    console.log(conn.refreshToken);
-    SFOauth.refreshToken = conn.refreshToken;
-    console.log(conn.instanceUrl);
-    SFOauth.instanceUrl = conn.instanceUrl;
+    // Save them to establish connection next time.	
+	router.db.get("organization").filter({sfdc_oauthState:state}).nth(0).assign({
+		sfdc_instanceUrl : conn.instanceUrl,
+		sfdc_refreshToken : conn.refreshToken,
+		sfdc_accessToken: conn.accessToken
+	}).write()
+
     console.log("User ID: " + userInfo.id);
     console.log("Org ID: " + userInfo.organizationId);
     // ...
     res.send('success'); // or your desired response
   });
 });
+
+server.get("/test/:oid",function(req,res){
+	let organization = router.db.get("organization").filter({"id": parseInt(req.params.oid)}).nth(0).value();
+	var conn = new jsforce.Connection({
+		oauth2 ,
+		instanceUrl : organization.sfdc_instanceUrl,
+		accessToken : organization.sfdc_accessToken,
+		refreshToken : organization.sfdc_refreshToken
+	  });
+	  conn.on("refresh", function(accessToken, res) {
+		// Refresh event will be fired when renewed access token
+		// to store it in your storage for next request
+		console.log("refresh access token", accessToken);
+		router.db.get("organization").filter({id: req.params.oid}).nth(0).assign({sfdc_accessToken:accessToken}).write()
+	  });
+
+	conn.identity(function(err, res2) {
+		if (err) { return console.error(err); }
+		res.send({
+			"user_id" : res2.user_id,
+			"organization_id" : res2.organization_id,
+			"username" : res2.username,
+			"display_name" : res2.display_name
+		})
+	  });
+})
 
 
 

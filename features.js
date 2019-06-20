583086b30f5f1bf42ffe424438107d7cf73b265c
Gael MOTTE <gael.motte@gmail.com>
2019-06-20 22:26:38 +0200

    checking fulll roundtrip

--- features.js
+++ features.js
@@ -4,167 +4,169 @@ const randomstring = require('randomstring')
 const shortid = require('shortid')
 
 
-module.exports = function(server, router, ss, oauth){
+module.exports = function (server, router, ss, oauth) {
 
-    server.use("/me",ss.authNMiddleware)
-    server.get("/me",function (req,res){
-        if(req.administrator){
-            res.send({
-                "isAdmin":true,
-                "username":req.administrator.username,
-                "organization_id":null,
-                "sfdc_instanceUrl":null
-            })
-        }else if(req.user || req.organization){
-            res.send({
-                "isAdmin":false,
-                "username":req.user ? req.user.username : null,
-                "organization_id":req.organization.id,
-                "sfdc_instanceUrl":req.organization.sfdc_instanceUrl
-            })
-        }else{
-            res.sendStatus(401)
-        }
-    });
+  server.use("/me", ss.authNMiddleware)
+  server.get("/me", function (req, res) {
+    if (req.administrator) {
+      res.send({
+        "isAdmin": true,
+        "username": req.administrator.username,
+        "organization_id": null,
+        "sfdc_instanceUrl": null
+      })
+    } else if (req.user || req.organization) {
+      res.send({
+        "isAdmin": false,
+        "username": req.user ? req.user.username : null,
+        "organization_id": req.organization.id,
+        "sfdc_instanceUrl": req.organization.sfdc_instanceUrl
+      })
+    } else {
+      res.sendStatus(401)
+    }
+  });
 
-    server.use("/testback",ss.authNMiddleware)
-    server.get("/testback",function(req,res){
-        res.send(req.organization.validateState);
-    });
-        
-    server.use("/test",ss.authNMiddleware)
-    server.get("/test",function(req,res){
-        
-        var conn = oauth.getConnection(req.organization);
-        try{
-            conn.identity(function(err, ret) {
-                if (err) {  
-                    console.error(err);
-                    res.status(500).send(err)
-                }else{
-                    //call rest endpoint to make sure reciprocal calls do work
-                        //generate temp state for echo service
-                   /* let a = router.db.get("organization").find({"id":req.organization.id}).value();
-                    a.validateState = randomstring.generate();
-                        //store in db
-                    router.db.get("organization").find({"id":req.organization.id}).assign(a).write();*/
+  server.use("/testback", ss.authNMiddleware)
+  server.get("/testback", function (req, res) {
+    res.send(req.organization.validateState);
+  });
 
-                    let result = {
-                        "user_id" : ret.user_id,
-                        "organization_id" : ret.organization_id,
-                        "username" : ret.username,
-                        "display_name" : ret.display_name
-                    }
+  server.use("/test", ss.authNMiddleware)
+  server.get("/test", function (req, res) {
+    var conn = oauth.getConnection(req.organization);
+    try {
+      conn.identity(function (err, ret) {
+        if (err) {
+          console.error(err);
+          res.status(500).send(err)
+        } else {
+          //call rest endpoint to make sure reciprocal calls do work
+          //generate temp state for echo service
+          let a = router.db.get("organization").find({ "id": req.organization.id }).value();
+          a.validateState = randomstring.generate();
+          //store in db
+          router.db.get("organization").find({ "id": req.organization.id }).assign(a).write();
 
-                        //call testback
-                    conn.apex.get("/hey/api/v1/test/", function(err2, ret2) {
-                        if (err) { 
-                            console.error(err2, ret2); 
-                            res.sendStatus(500, err2)
-                        }else{
-                            console.log("call successfull: " , ret2);
-                            //check for errors found at apex side :
-                                // - NamedCredNotFound ?
-                                // - Unauthorized ?
+          let result = {
+            "user_id": ret.user_id,
+            "organization_id": ret.organization_id,
+            "username": ret.username,
+            "display_name": ret.display_name
+          }
 
-                            //compare validateState
+          //call testback
+          conn.apex.get("/hey/api/v1/test/", function (err2, ret2) {
+            if (err) {
+              console.error(err2, ret2);
+              res.sendStatus(500, err2)
+            } else {
+              console.log("call successfull: ", ret2);
+              //check for errors found at apex side :
+              // - NamedCredNotFound ?
+              // - Unauthorized ?
 
-                            // clear value if ok
+              if (ret2.err == "APPEXCHANGE_NOT_INSTALLED") {
+                result.error = ret2.err
+              } else if (ret2.err == "NAMED_CREDENTIALS_UNAUTHORIZED") {
+                result.error = ret2.err
+              } else if (ret2.err) {
+                result.error = "UNKNOWN_ERROR"
+              } else {
+                if (result.validateState == a.validateState) {
+                  //validation passes
+                  delete a.validateState;
+                  router.db.get("organization").find({ "id": req.organization.id }).assign(a).write();
 
-                            // build link if not ok
-                            result.what=ret2;
-                            
-                
-                        }            
-                    });
-                    res.send(result);
-
-                        
-
-
-                    
+                } else {
+                  //validaiton fails ?? We are not refering to the correct org
+                  result.error = "WRONG_ORG"
                 }
-
-            
-            });
-        }catch(e){
-            res.status(500).send(e);
+              }
+            }
+            res.send(result);
+          });
         }
-    })
+      });
 
-    server.use("/installpackage",ss.authNMiddleware)
-    server.get("/installpackage", (req,res)=>{
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  })
 
-        let a = router.db.get("organization").find({id:req.organization.id}).value()
-        a.ncUsername = req.organization.id; 
-        a.ncPassword = randomstring.generate(32);
-        router.db.get("organization").find({id:req.organization.id}).assign(a).write()
+  server.use("/installpackage", ss.authNMiddleware)
+  server.get("/installpackage", (req, res) => {
 
-        res.send({
-            "ncUsername" : req.organization.ncUsername,
-            "ncPassword" : req.organization.ncPassword,
-            "redirectTo":req.organization.sfdc_instanceUrl+"/packagingSetupUI/ipLanding.app?apvId="+ process.env.PACKAGE_ID,
-        });
-    })
+    let a = router.db.get("organization").find({ id: req.organization.id }).value()
+    a.ncUsername = req.organization.id;
+    a.ncPassword = randomstring.generate(32);
+    router.db.get("organization").find({ id: req.organization.id }).assign(a).write()
 
-    server.use("/work/leads",ss.authNMiddleware)
-    server.get("/work/leads",(req,res)=>{
-        //send lead to SFDC
-        let conn = oauth.getConnection(req.organization);
-        try{
-            conn.apex.get("/hey/api/v1/lead/", function(err, ret) {
-                if (err) { 
-                    console.error(err, ret); 
-                    res.sendStatus(500, err)
-                }else{
-                    console.log("call successfull: " + ret);
-                
-                    res.send(ret);
-        
-                }            
-            });
-        }catch(e){
-            res.status(500).send(e);
-        }
+    res.send({
+      "ncUsername": req.organization.ncUsername,
+      "ncPassword": req.organization.ncPassword,
+      "redirectTo": req.organization.sfdc_instanceUrl + "/packagingSetupUI/ipLanding.app?apvId=" + process.env.PACKAGE_ID,
     });
+  })
 
-    server.use("/work/lead",ss.authNMiddleware)
-    server.post("/work/lead",(req,res)=>{
-        //send lead to SFDC
-        let conn = oauth.getConnection(req.organization);
-        let lead = {
-            id : shortid.generate(),
-            FirstName : req.body.leadFirstName,
-            LastName : req.body.leadLastName,
-            Company : req.body.leadCompany,
-            Description : req.body.leadDescription        
-        };
+  server.use("/work/leads", ss.authNMiddleware)
+  server.get("/work/leads", (req, res) => {
+    //send lead to SFDC
+    let conn = oauth.getConnection(req.organization);
+    try {
+      conn.apex.get("/hey/api/v1/lead/", function (err, ret) {
+        if (err) {
+          console.error(err, ret);
+          res.sendStatus(500, err)
+        } else {
+          console.log("call successfull: " + ret);
 
-        console.log(lead)
-        try{
-            conn.sobject("Lead").create({
-                FirstName : lead.FirstName,
-                LastName : lead.LastName,
-                Company : lead.Company,
-                Description: lead.Description,
-                Hey_Id__c : lead.id
-            }, function(err, ret) {
-                if (err || !ret.success) { 
-                    console.error(err, ret); 
-                    res.sendStatus(500, err)
-                }else{
-                    console.log("Created record id : " + ret.id);
-                    lead.sfdcId = ret.id;
-                    router.db.get("leads").push(lead).write();
-        
-                    res.send(lead);
-        
-                }            
-            });
-        }catch(e){
-            res.status(500).send(e);
+          res.send(ret);
+
         }
-        
-    });
+      });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+
+  server.use("/work/lead", ss.authNMiddleware)
+  server.post("/work/lead", (req, res) => {
+    //send lead to SFDC
+    let conn = oauth.getConnection(req.organization);
+    let lead = {
+      id: shortid.generate(),
+      FirstName: req.body.leadFirstName,
+      LastName: req.body.leadLastName,
+      Company: req.body.leadCompany,
+      Description: req.body.leadDescription
+    };
+
+    console.log(lead)
+    try {
+      conn.sobject("Lead").create({
+        FirstName: lead.FirstName,
+        LastName: lead.LastName,
+        Company: lead.Company,
+        Description: lead.Description,
+        Hey_Id__c: lead.id
+      }, function (err, ret) {
+        if (err || !ret.success) {
+          console.error(err, ret);
+          res.sendStatus(500, err)
+        } else {
+          console.log("Created record id : " + ret.id);
+          lead.sfdcId = ret.id;
+          router.db.get("leads").push(lead).write();
+
+          res.send(lead);
+
+        }
+      });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+
+  });
 
 }
